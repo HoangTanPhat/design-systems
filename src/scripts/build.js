de8fa5b78cba35f44bacc5f68dfc76b062db6ce6
Hoang Tan Phat <hoangtanphat97.xm@gmail.com>
2023-06-06 19:22:12 +0700

    init design-systems

---
+++ src/scripts/build.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const sass = require("node-sass");
+
+const getComponents = () => {
+  let allComponents = [];
+
+  const types = ["atoms", "molecules", "organisms"];
+
+  types.forEach((type) => {
+    const allFiles = fs.readdirSync(`src/${type}`).map((file) => ({
+      input: `src/${type}/${file}`,
+      output: `src/lib/${file.slice(0, -4) + "css"}`,
+    }));
+
+    allComponents = [...allComponents, ...allFiles];
+  });
+
+  return allComponents;
+};
+
+const compile = (fromPath, fileName) => {
+  const result = sass.renderSync({
+    data: fs.readFileSync(path.resolve(fromPath)).toString(),
+    outputStyle: "expanded",
+    outFile: "global.css",
+    includePaths: [path.resolve("src")],
+  });
+
+  fs.writeFileSync(fileName, result.css.toString());
+};
+
+compile("src/global.scss", "src/lib/global.css");
+
+console.log(getComponents());
+
+getComponents().forEach((component) => {
+  compile(component.input, component.output);
+});
