35c477322251f164b2e2563c88075fa426debe52
Hoang Tan Phat <hoangtanphat97.xm@gmail.com>
2023-06-07 22:36:55 +0700

    fix: fix build failed

--- packages/scss/src/scripts/build.js
+++ packages/scss/src/scripts/build.js
@@ -5,7 +5,7 @@ const sass = require("node-sass");
 const getComponents = () => {
   let allComponents = [];
 
-  const types = ["atoms", "molecules", "organisms"];
+  const types = ["atoms"];
 
   types.forEach((type) => {
     const allFiles = fs.readdirSync(`src/${type}`).map((file) => ({
